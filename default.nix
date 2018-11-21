1bbadf4fd7f37aae8a23a939edbd82b4b59883bf
Dave Laing <dave.laing.80@gmail.com>
2018-11-21 14:39:23 +1000

    Removes GHC 8.6.1 for now

--- default.nix
+++ default.nix
@@ -22,7 +22,6 @@ let
       rev = "97ef00aa45c70213a4f0ce348a2208e3f482a7e3";
       sha256 = "0qm0ay49wc0frxs6ipc10xyjj654b0wgk0b1hzm79qdlfp2yq0n5";
     };
-
   };
 
   modifiedHaskellPackages = haskellPackages.override {
